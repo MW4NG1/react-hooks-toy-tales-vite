92af55df13942a2cbcd1859d67c866909428cd1d
MW4NG1 <msage3803@gmail.com>
2026-08-05 22:34:59 +0300

    Add delete functionality for toys in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,6 +37,16 @@ function App() {
       });
   }
 
+  // Delete a toy from the database and state
+function handleDeleteToy(id) {
+  fetch(`http://localhost:3001/toys/${id}`, {
+    method: "DELETE",
+  }).then(() => {
+    const updatedToys = toys.filter((toy) => toy.id !== id);
+    setToys(updatedToys);
+  });
+}
+
   return (
     <>
       <Header />
